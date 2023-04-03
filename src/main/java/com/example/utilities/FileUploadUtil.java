83fdbea98061349e3d785666d21491e5d4eebf98
celia <celia.cava18ruiz@gmail.com>
2023-04-03 16:48:51 +0200

    todo

---
+++ src/main/java/com/example/utilities/FileUploadUtil.java
@@ -0,0 +1,40 @@
+package com.example.utilities;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import org.apache.commons.lang3.RandomStringUtils;
+
+@Component
+public class FileUploadUtil {
+
+    public String saveFile(String fileName, MultipartFile multipartFile)
+            throws IOException {
+        Path uploadPath = Paths.get("Files-Upload");
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        String fileCode = RandomStringUtils.randomAlphanumeric(8);
+
+        //Este try-catch se llama Try-with-resources
+        //Los recursos que se pueden manejar son los que implementa la interfaz closeable
+        
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileCode + "-" + fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save file: " + fileName, ioe);
+        } 
+
+        return fileCode;
+    }
+
+}
